8f07dff1578fcb855c23540814b54c335daf97c1
João Victor (Jovi) <nean@protonmail.com>
2022-11-24 03:28:20 -0300

    Mudança extremamente importante no controller do projeto base

--- hello-world/src/app.controller.ts
+++ hello-world/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/admin')
+  @Redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 301)
+  getAdmin() {}
 }
